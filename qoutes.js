2079ce88e87de978f5179d289a15d88b5db8875a
Jessie <jessiegonzo2020@gmail.com>
2020-06-11 23:02:03 -0700

    Finished adding 12 qoutes, you can add as many as you need

--- qoutes.js
+++ qoutes.js
@@ -1,3 +1,4 @@
+//Carl said to create this qoute.js file, and all function classes(.) and ID(#)s would communicate.
 // ALWAYS INCLUDE document.addEventListener('DOMContentLoaded', function () {}) FUNCTION WHEN STARTING ANY JS FUNCTION ON THIS FILE. 
 //THIS FIRST LINE OF CODE IS NEEDED TO TELL THE DOM TO RUN THIS JS BEFORE RENDIRING THE PAGE
 //ITS ONE OF THOSE RULES YOU HAVE TO LIVE WITH
@@ -16,17 +17,17 @@ var x = document.getElementById("qoutesBtn");
         function tfunction() {
             //Source for borrowed logic: starting from "const qoutes =" ending at randomQoute console.log (https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array)
             const qoutes = ["If you don't Have TIME, You Don't Have PRIORITIES -Tim Ferris",
-             "Anger is fear shown publicly -Tim Ferris", 
-             "Discipline will set you free -Jacko Williams", 
-             "5 P's Proper, Preperation, Prevents, Poor, Performance -Vicente R.", 
-             "The unexamined life is not worth living -Socrates", 
-             "June", 
-             "July", 
-             "August", 
-             "September", 
-             "October", 
-             "November", 
-             "December"];
+             "Anger is fear shown publicly  -Tim Ferris", 
+             "Discipline will set you free  -Jocko Williams", 
+             "The 5 P's: Proper, Preperation, Prevents, Poor, Performance ", 
+             "A life not examined is a life not worth living -Socrates", 
+             "Lifes Purpose: Healthy Lifestyle, Loving Family, Financial Independence", 
+             "Seneca: We suffer more often in imagination, then in reality", 
+             "Anxiety is living in the future, Depression is living in the past, Breathe and remember to be PRESENT", 
+             "How to combat Anxiety: Tap into your pre-frontal cortex by making lists, schedule time to work on whats importatn but not yet due", 
+             "3 Rules of Life: Drive your own car. Drive around the next corner. Leave what's in the rear view mirror, in the rear view mirror.", 
+             "The way you view other races, is a clear determinent to how successfull you will be - Dr. Paul", 
+             "Thank you Carl and Justin and Vets In Tech for helping me learn to build this website!!"];
             const randomQoute = qoutes[Math.floor(Math.random() * qoutes.length)];
 
             //This console log shows you in dev tools the above is working.
