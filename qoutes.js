40d1085b28a30dbae1d5a66b8051ddf13e4cc2a2
Jessie <jessiegonzo2020@gmail.com>
2020-06-12 18:23:46 -0700

    Added 4 more qoutes

--- qoutes.js
+++ qoutes.js
@@ -27,7 +27,13 @@ var x = document.getElementById("qoutesBtn");
              "How to combat Anxiety: Tap into your pre-frontal cortex by making lists, schedule time to work on whats importatn but not yet due", 
              "3 Rules of Life: Drive your own car. Drive around the next corner. Leave what's in the rear view mirror, in the rear view mirror.", 
              "The way you view other races, is a clear determinent to how successfull you will be - Dr. Paul", 
-             "Thank you Carl and Justin and Vets In Tech for helping me learn to build this website!!"];
+             "Thank you Carl and Justin and Vets In Tech for helping me learn to build this website!!",
+             "Things stand outside of us, themselves by themselves, neither knowing anything of themselves nor expressing any judgment -Marcus Aurelius",
+             "We should always be asking ourselves: “Is this something that is, or is not, in my control? - Epictetus",
+             "Cling tooth and nail to the following rule: not to give in to adversity, never to trust prosperity, and always take full note of fortune’s habit of behaving just as she pleases, treating her as if she were actually going to do everything it is in her power to do. Whatever you have been expecting for some time comes as less of a shock. - Seneca",
+             "It is not the man who has too little that is poor, but the one who hankers after more.― Lucius Annaeus Seneca",
+             "Never let the future disturb you. You will meet it, if you have to, with the same weapons of reason which today arm you against the present. ― Marcus Aurelius, Meditations",
+             "We suffer more often in imagination than in reality. ― Lucius Annaeus Seneca",];
             const randomQoute = qoutes[Math.floor(Math.random() * qoutes.length)];
 
             //This console log shows you in dev tools the above is working.
